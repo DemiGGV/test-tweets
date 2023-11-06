a637760492280db2d0126f133f8cfaffa8b7564e
Heorhii Honcharov <0019@ukr.net>
2023-11-06 13:45:47 +0100

    adding basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { GlobalCSS } from 'GlobalCSS';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test-tweets">
       <GlobalCSS />
       <App />
     </BrowserRouter>

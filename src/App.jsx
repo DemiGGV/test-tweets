850cb258278979f25f696a7343f7ff29f1ccabef
Heorhii Honcharov <0019@ukr.net>
2023-11-07 21:26:07 +0100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export const App = () => {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Tweets />} />
-        <Route path="/tweets" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </div>
--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -127,7 +127,9 @@ const Tweets = () => {
           />
           <BackButton label="Back" />
         </>
-      ) : null}
+      ) : (
+        'Tost'
+      )}
     </>
   );
 };
